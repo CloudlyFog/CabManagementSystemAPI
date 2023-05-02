9ee1246f98c4128272c4988df49da1b94fd9ec5a
mxdnxght <cloudyfg@gmail.com>
2023-05-02 22:03:53 +0300

    stage changes.

---
+++ CabManagementSystemTest/UnitTest1.cs
@@ -0,0 +1,31 @@
+using CabManagementSystem.Models;
+using CabManagementSystem.Services.Repositories;
+
+namespace CabManagementSystemTest
+{
+    public class Tests
+    {
+        private OrderRepository _orderRepository;
+        [SetUp]
+        public void Setup()
+        {
+            _orderRepository = new();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var order = GetOrder();
+            _orderRepository.Create(order);
+            Assert.Pass();
+        }
+
+        private Order GetOrder()
+        {
+            var car = new Car
+            {
+
+            }
+        }
+    }
+}
