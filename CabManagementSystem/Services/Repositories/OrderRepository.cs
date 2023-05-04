c13d6a85c7185e8b4b53f505cfaa710e762a5084
mxdnxght <cloudyfg@gmail.com>
2023-05-04 20:38:28 +0300

    Updated to new non-release version of BankSystem and updated logic in OrderRepository.cs.

--- CabManagementSystem/Services/Repositories/OrderRepository.cs
+++ CabManagementSystem/Services/Repositories/OrderRepository.cs
@@ -1,23 +1,33 @@
 ﻿using BankSystem7.Models;
+using BankSystem7.Services.Configuration;
 using BankSystem7.Services.Interfaces;
 using CabManagementSystem.Data;
 using CabManagementSystem.Models;
+using CabManagementSystem.Services.Abstract;
+using CabManagementSystem.Services.Configuration;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace CabManagementSystem.Services.Repositories;
 
-public sealed class OrderRepository : IRepository<Order>
+public sealed class OrderRepository : OptionsUpdater, IRepository<Order>
 {
     private readonly CabContext _cabContext;
-    public OrderRepository()
+
+    public OrderRepository(CabManagementOptions options)
     {
+        UpdateOptions(options);
         _cabContext = new CabContext();
     }
+
     public IQueryable<Order> All =>
         _cabContext.Orders
+        .Include(x => x.User)
+        .ThenInclude(x => x.Orders)
         .Include(x => x.Car)
+        .ThenInclude(x => x.Driver)
         .Include(x => x.Driver)
+        .ThenInclude(x => x.Car)
         .AsNoTracking() ?? Enumerable.Empty<Order>().AsQueryable();
 
     public ExceptionModel Create(Order item)
@@ -25,8 +35,10 @@ public sealed class OrderRepository : IRepository<Order>
         if (item?.Car is null || item?.Driver is null)
             return ExceptionModel.EntityIsNull;
 
-        if (!Exist(x => x.Id == item.Id))
+        if (Exist(x => x.Id == item.Id))
             return ExceptionModel.EntityNotExist;
+
+        SetOrderDetails(item);
 
         _cabContext.ChangeTracker.Clear();
         _cabContext.Orders.Add(item);
@@ -62,5 +74,17 @@ public sealed class OrderRepository : IRepository<Order>
     public ExceptionModel Update(Order item)
     {
         throw new NotImplementedException();
+    }
+
+    private void SetOrderDetails(Order? order)
+    {
+        order.Car.IsBusy = true;
+        order.Driver.IsBusy = true;
+    }
+
+    protected override void UpdateOptions(ConfigurationOptions options)
+    {
+        UpdateDatabaseName(options.DatabaseName);
+        base.UpdateOptions(options);
     }
 }
