3c321ffc1cf273c49c4a504b06ef0fd146d7a88b
mxdnxght <cloudyfg@gmail.com>
2023-05-05 11:56:52 +0300

    Added methods to avoid duplication and/or changing data that we won't change.

--- CabManagementSystem/Data/CabContext.cs
+++ CabManagementSystem/Data/CabContext.cs
@@ -18,4 +18,25 @@ public class CabContext : GenericDbContext<CabUser, Card, BankAccount, Bank, Cre
 
     public DbSet<Order> Orders { get; set; }
     public DbSet<Car> Cars { get; set; }
+
+    public void AvoidOrderChanges(Order order)
+    {
+        if (order is null)
+            return;
+        Entry(order.Car).State = EntityState.Unchanged;
+        Entry(order.Driver).State = EntityState.Unchanged;
+        Entry(order.User).State = EntityState.Unchanged;
+    }
+
+    public void AvoidChanges<T>(T item, Type[] types)
+    {
+        if (item is null || types is null || types.Length == 0)
+            return;
+
+        foreach (var prop in item.GetType().GetProperties())
+        {
+            if (types.Any(x => x == prop.GetType()))
+                Entry(prop.GetValue(item)).State = EntityState.Unchanged;
+        }
+    }
 }
